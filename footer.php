2cc89865141b117598dd21f2803f0f0154f70946
Logan K <lkirkend@ucsd.edu>
2023-02-23 22:16:27 -0800

    Changed the template's branding to KSDT_WI23

--- footer.php
+++ footer.php
@@ -13,16 +13,16 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ksdt_wi23' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'https://ksdt.ucsd.edu', 'ksdt_wi23' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ksdt_wi23' ), 'WordPress' );
+				printf( esc_html__( 'Unproudly powered by %s', 'ksdt_wi23' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ksdt_wi23' ), 'ksdt_wi23', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ksdt_wi23' ), 'ksdt_wi23', '<a href="https://ksdt.ucsd.edu">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->

a99bd1619a0948cf153b652963a667cbd6943602
Ddg3 <zachlawrence2003@hotmail.com>
2023-03-09 19:13:23 -0800

    moved spinitron directories all into /spinitron

--- page-schedule.php
+++ page-schedule.php
@@ -10,8 +10,8 @@ get_header();
 	<section class="about_descr mt-50">
 		<div style="height: 650px;padding-bottom: 25px;" class="container">
 			<div class="row center">
-                <div id="spin-recent"><?php include(__DIR__ . '/recent.php') ?></div>
-				<div id="spin-upcoming"><?php include(__DIR__ . '/upcoming-shows.php') ?></div>
+                <div id="spin-recent"><?php include(__DIR__ . '/spinitron/widgets/recent-spins.php') ?></div>
+				<div id="spin-upcoming"><?php include(__DIR__ . '/spinitron/widgets/upcoming-shows.php') ?></div>
 			</div>
 		</div>
 	</section>

3ebd139a3181dd746f9aa009b7f7ffb8aa948665
lkkir <lkirkend@ucsd.edu>
2023-03-30 19:40:12 -0700

    SVG Files for main page, commented CSS, updated spacing in header

--- header.php
+++ header.php
@@ -14,11 +14,10 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <meta name="author" content="KSDT Radio">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
 
@@ -28,15 +27,13 @@
         <ul class="navbar">
             <?php $main_menu = wp_get_nav_menu_items('Main'); ?>
                 <?php foreach ((array) $main_menu as $key => $menu_item): ?>
-                <?php
-                $title = strtoupper($menu_item->title);
-                $url = $menu_item->url;
-                ?>
+                    <?php
+                    $title = strtoupper($menu_item->title);
+                    $url = $menu_item->url;
+                    ?>
                 <li class="nav-item">
                     <a href="<?php echo $url; ?>"><?php echo $title; ?></a>
                 </li>
-                <?php endforeach; ?>
+            <?php endforeach; ?>
         </ul>
     </div>
-</body>
-

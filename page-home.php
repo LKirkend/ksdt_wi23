059f35ba75d37d7c35dfaf5eceec424fc9739b20
lkkir <lkirkend@ucsd.edu>
2023-04-05 04:44:50 -0700

    Finished homepage styling, still needs spinitron/insta embed. Added css for stickers, comments.

--- page-home.php
+++ page-home.php
@@ -17,15 +17,15 @@
 get_header();
 ?>
 <div class="container-fluid px-sm-5" id="main-header">
-    <div class="row gx-sm-5 pt-sm-5">
-        <div class="col-sm py-2">
+    <div class="row gx-md-5 pt-sm-5">
+        <div class="col-md py-2">
             <img id="home-logo">
         </div>
-        <div class="col-sm card mx-auto ms-lg-5 ms-sm-auto p-0" id="tune-in-box">
+        <div class="col-md card mx-auto ms-lg-5 ms-sm-auto p-0" id="tune-in-box">
             <div class="row">
                 <div class="col">
                     <h1 class="card-title d-none d-lg-block mx-auto text-center" id="tune-in">tune in now:</h5>
-                    <h1 class="card-title fs-1 d-none d-sm-block d-lg-none mx-auto text-center" id="tune-in">tune in now:</h5>  
+                    <h1 class="card-title fs-1 d-none d-md-block d-lg-none mx-auto text-center" id="tune-in">tune in now:</h5>  
                     
                 </div>
             </div>
@@ -35,11 +35,61 @@ get_header();
                 </div>
                 <div class="col py-3" id="show-current">
                     <!-- <?php include(__DIR__ . '/spinitron/widgets/current-show.php') ?> -->
-                    <span id="show">show name<br></span>
-                    <span id="time">12:00pm-1:30pm<br></span>
-                    <span id="song">song name + artist name</span>
+                    <span class="spinitron pt-sm-5 pt-md-0" id="show">show name<br></span>
+                    <span class="spinitron" id="time">12:00pm-1:30pm<br></span>
+                    <span class="spinitron" id="song">song name + artist name</span>
                 </div>
             </div> 
+        </div>
+    </div>
+    <!-- scroll to learn more -->
+    <div class="row">
+        <div class="col">
+            <img id="scroll">
+        </div>
+    </div>
+    <!-- instagram imbed -->
+    <div class="row mx-auto" id="insta-box">
+        <div class="row p-0 pt-3 mx-auto">
+            <div class="col">
+                <img id="welcome">
+            </div>
+        </div>
+        <!-- row 1 -->
+        <div class="row p-0 px-md-2 px-lg-4 py-md-2 py-lg-4 mx-auto">
+            <div class="col-sm px-0 py-0 pe-md-2 pe-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+            <div class="col-sm px-0 py-0 px-md-2 px-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+            <div class="col-sm px-0 py-0 ps-md-2 ps-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+        </div>
+        <!-- row 2 -->
+        <div class="row p-0 px-md-2 px-lg-4 py-md-2 py-lg-4 mx-auto">
+            <div class="col-sm px-0 py-0 pe-md-2 pe-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+            <div class="col-sm px-0 py-0 px-md-2 px-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+            <div class="col-sm px-0 py-0 ps-md-2 ps-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+        </div>
+        <!-- row 3 -->
+        <div class="row p-0 px-md-2 px-lg-4 pt-md-2 pt-lg-4 pb-md-2 pb-lg-3 mx-auto">
+            <div class="col-sm px-0 py-0 pe-md-2 pe-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+            <div class="col-sm px-0 py-0 px-md-2 px-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 py-1 py-md-0">
+            </div>
+            <div class="col-sm px-0 py-0 ps-md-2 ps-lg-5">
+                <img id="botb" class="w-100 px-2 px-md-0 pt-1 pb-2 py-md-0">
+            </div>
         </div>
     </div>
 </div>

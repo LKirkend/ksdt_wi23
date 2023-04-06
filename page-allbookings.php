f39900883def889478d7240a6114b8dfcf5c375e
Logan Kirkendall <lkirkend@ucsd.edu>
2023-04-06 02:28:36 -0700

    Fixed scheduler spacing bugs, added admin page

--- page-allbookings.php
+++ page-allbookings.php
@@ -1,21 +1,17 @@
 <?php get_header(); ?>
-  <style>
-    iframe {
-      width: 100%;
-      height: 600px
-    }
-  </style>
-<body>
-  <div class="content">
-    <div style="height: 2000px; padding-top: 50px" class="container">'
-      <div>
-        <?php echo do_shortcode('[CP_APP_HOUR_BOOKING_ADMIN]')?>
-      </div>
-      <div class="row center">
-        <a href="https://ksdt.ucsd.edu/wp-admin/admin.php?page=cp_apphourbooking&cal=1&schedule=1&calendarview=1&r=0.3494832573835398" target="_blank" class="button-listen-live" style="font-size: 20px;">Current Schedule&nbsp;</a>
-        <a href="https://ksdt.ucsd.edu/wp-admin/admin.php?page=cp_apphourbooking" target="_blank" class="button-listen-live" style="font-size: 20px;">Admin Dashboard&nbsp;</a>
-      </div>
+
+<div class="container-flex" style="height:1300px;">  
+  <div class="row pt-5">
+    <div class="col-lg display-inline ms-5 text-center">
+      <a href="https://ksdt.ucsd.edu/wp-admin/admin.php?page=cp_apphourbooking&cal=1&schedule=1&calendarview=1&r=0.3494832573835398" class="admin-btn py-5">Current Schedule&nbsp;</a>
+    </div>
+    <div class="col-lg display-inline me-5 text-center">
+      <a href="https://ksdt.ucsd.edu/wp-admin/admin.php?page=cp_apphourbooking" class="admin-btn py-5">Admin Dashboard&nbsp;</a>
     </div>
   </div>
-  <?php get_footer(); ?>
-</body>
+  <div class="row rainbox my-5 mx-5" style="background-color: var(--transblack2)">
+    <?php echo do_shortcode('[CP_APP_HOUR_BOOKING_ADMIN]')?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
--- page-scheduler.php
+++ page-scheduler.php
@@ -10,12 +10,12 @@
 get_header();
 ?>
 <div class="container-fluid">
-	<div class="row w-100 mx-auto my-5" id="scheduler-box">
+	<div class="row w-100 mx-auto my-5 position-relative z-1" id="scheduler-box">
 		<div class="row mx-auto py-5" >
 			<?php echo do_shortcode('[CP_APP_HOUR_BOOKING id="1"]')?>
 		</div>
 
-		<div class="row py-5" id="appts">
+		<div class="row py-5 mx-auto text-center" id="appts">
 			<?php echo do_shortcode('[CP_APP_HOUR_BOOKING_LIST calendar="1" from="today" to="today +7 days" fields="weekday, DATE, TIME, fieldname2, EDITLINK" onlycurrentuser="yes"]')?>
 		</div>
 	</div>

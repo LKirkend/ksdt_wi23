6f96bfbe782efdb3f2cd64cc75d856001b2357b7
Logan Kirkendall <lkirkend@ucsd.edu>
2023-04-11 12:37:11 -0700

    Fix button stretching

--- page-home.php
+++ page-home.php
@@ -25,7 +25,7 @@ get_header();
             </div>
             <div class="row h-100 mx-0 overflow-y-scroll overflow-x-hidden" id="current-bottom">
                 <div class="play-btn col-5 px-0 px-lg-3 h-lg-100 my-auto mh-100 py-sm-4">
-                    <img id="radio-toggle" src='<?php echo get_stylesheet_directory_uri(); ?> . /img/logos/playbutton.svg' onclick="togglePlay()" class="play-button mx-0 h-100 pe-lg-0 h-lg-100 mh-100 pb-sm-5 my-5 my-md-0" onerror="this.classList.add('play-button2');">
+                    <img id="radio-toggle" src='<?php echo get_stylesheet_directory_uri() . "/img/logos/playbutton.svg"?>' onclick="togglePlay()" class="play-button mx-0 h-100 pe-lg-0 h-lg-100 mh-100 pb-sm-5 my-5 my-md-0 pb-lg-0" onerror="this.classList.add('play-button2');">
                 </div>
                 <div class="col py-0 py-sm-3 px-0 h-auto my-auto mt-lg-3 mb-lg-0" id="show-current">
                     <?php include(__DIR__ . '/spinitron/widgets/current-show.php') ?>

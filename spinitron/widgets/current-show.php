959302c49a18c896ba13d03db25a6fb73e32163e
zclawr <zclawrence@ucsd.edu>
2023-04-10 17:32:59 -0700

    added href redirect for off-air homepage and broadcast page WIP

--- spinitron/widgets/current-show.php
+++ spinitron/widgets/current-show.php
@@ -6,8 +6,6 @@ $spin = $client->search('spins', ['count' => 1])['items'][0];
 $start = (new DateTime($show['start']))->setTimezone(new DateTimeZone($show['timezone'] ?? 'America/Los_Angeles'));
 $end = (new DateTime($show['end']))->setTimezone(new DateTimeZone($show['timezone'] ?? 'America/Los_Angeles'));
 $currentTime = (new DateTime('now', new DateTimeZone($show['timezone'])))->format('d:ga');
-
-echo strcmp($currentTime, $start->format('d:ga')) == 0;
 ?>
 
 <?php if(strcmp($currentTime, $start->format('d:ga')) == 0) { ?>
@@ -15,8 +13,8 @@ echo strcmp($currentTime, $start->format('d:ga')) == 0;
     <span class="spinitron" id="time"><?= $start->format('g:ia') . ' - ' . $end->format('g:ia')?><br></span>
     <span class="spinitron" id="song"><?= htmlspecialchars($spin['song'], ENT_NOQUOTES) ?> by <?= htmlspecialchars($spin['artist'], ENT_NOQUOTES) ?></span>
 <?php } else { ?>
-    <span class="spinitron pt-sm-4 pt-md-0 mw-fit" id="show">Off The Air<br></span>
-    <span class="spinitron" id="song">Tune in from <?= $start->format('g:ia') . ' - ' . $end->format('g:ia')?> for <?= htmlspecialchars($show['title'], ENT_NOQUOTES) ?></span>
+    <span class="spinitron pt-sm-4 pt-md-0 mw-fit" id="show-off-air">Off The Air<br></span>
+    <span class="spinitron" id="show-upcoming" onclick="redirectClick(this)" href="https://spinitron.com/KSDT/show/<?=$show['id']?>">Tune in from <?= $start->format('g:ia') . ' - ' . $end->format('g:ia')?> for <?= htmlspecialchars($show['title'], ENT_NOQUOTES) ?></span>
 <?php } ?>
 
 <script>
--- spinitron/widgets/upcoming-shows.php
+++ spinitron/widgets/upcoming-shows.php
@@ -2,12 +2,13 @@
 include __DIR__ . '/../getClient.php';
 /** @var SpinitronApiClient $client */
 $result = $client->search('shows', ['count' => 12]);
+$dj = $client->getPersonaFromShow($show);
 ?>
 
 <?php foreach ($result['items'] as $show): ?>
     <p><?= (new DateTime($show['start']))
             ->setTimezone(new DateTimeZone($show['timezone'] ?? 'America/Los_Angeles'))
             ->format('g:ia') ?>
-        <b><?= htmlspecialchars($show['title'], ENT_NOQUOTES) ?></b>
-        with <?= htmlspecialchars($client->getPersonaFromShow($show)['name'], ENT_NOQUOTES) ?>
+        <b><a href="https://spinitron.com/KSDT/show/<?=$show['id']?>"><?= htmlspecialchars($show['title'], ENT_NOQUOTES) ?></a></b> 
+        with <?= htmlspecialchars($dj['name'], ENT_NOQUOTES) ?>
 <?php endforeach ?>

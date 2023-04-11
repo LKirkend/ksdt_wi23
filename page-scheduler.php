9114d5b7784e3f8db51cba2de8ed0e967e37ea1b
Logan Kirkendall <lkirkend@ucsd.edu>
2023-04-11 16:18:58 -0700

    Homepage styling, scheduler styling / readability

--- page-scheduler.php
+++ page-scheduler.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-<div class="container-fluid">
+<div class="container-fluid px-0 w-100">
 	<div class="row w-100 mx-auto my-5 position-relative z-1 rainbox" id="scheduler-box">
 		<div class="row mx-auto py-5" >
 			<?php echo do_shortcode('[CP_APP_HOUR_BOOKING id="1"]')?>

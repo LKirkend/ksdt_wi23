40ccfb20ba2b0ba79cc71a438edc07a583e25b41
Logan Kirkendall <lkirkend@ucsd.edu>
2023-04-11 14:21:13 -0700

    Broadcast page spinning record when there's no album art

--- spinitron/widgets/recent-spins.php
+++ spinitron/widgets/recent-spins.php
@@ -6,8 +6,12 @@ $result = $client->search('spins', ['count' => 3]);
 foreach ($result['items'] as $spin): ?>
     <div class="row">
         <div class="col-sm-5">
-            <img src="<?=$spin['image']?>">
-            <!-- Todo: get picture of album art / artist photo -->
+            <?php $img = $spin['image'];
+            if($img == null){
+            ?>  <img class="spinning-record" style="width:170px; height:170px;"> <?php
+            } else{ ?>
+                <img src="<?=$img?>">
+            <?php }?>
         </div>
         <div class="col-sm pb-5">
             <span class="recent-date">

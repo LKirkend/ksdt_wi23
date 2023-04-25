9867f314a548aa9efef99b1db1ae0b04be48452b
Catherine Zhang <caz002@ucsd.edu>
2023-04-24 22:09:38 -0700

    These are the changes we made on mon april 24th

---
+++ page-sports.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Sports Broadcasting Page
+ * 
+ */
+get_header();
+?>
+<div class="container-flex">
+    <div class="row mx-3 w-100 text-center">
+        <div class="col-lg mb-2 mt-5 my-lg-5 ms-lg-5 rainbox my-5 ms-1 me-1 w-100" style= "background: var(--transblack2);">
+            <h1>Sports Broadcasting</h1>
+            <div class="row overflow-scroll flex-nowrap text-center gy-5 w-100">
+                <div class="col"> 
+                    <p class="fs-6 lh-1 "> Men's<br>Baseball</p>
+                </div>
+                <div class="col"> 
+                    <p class="fs-6 lh-1 ">Men's<br>Basketball</p>
+                </div>
+                <div class="col">  
+                    <p class="fs-6 lh-1 ">Women's<br>Basketball</p>
+                </div>
+                <div class="col"> 
+                    <p class="fs-6 lh-1 ">Men's<br>Volleyball</p>
+                </div>
+                <div class="col"> 
+                    <p class="fs-6 lh-1 ">Women's<br>Volleyball</p>
+                </div>
+                <div class="col"> 
+                    <p class="fs-6 lh-1 ">Men's<br>Soccer</p>
+                </div>
+                <div class="col"> 
+                    <p class="fs-6 lh-1 ">Women's<br>Soccer</p>
+                </div>
+            </div>
+            <div class="row text-center w-100">
+                <div class="col"><p>Here is where we will input text for the sports broadcasting page.</div>
+            </div> 
+        </div>
+        <div class="col-sm rainbox my-5 ms-1 me-5 w-100" style= "background: var(--transblack2);"><h1>Video</h1></div>
+    </div>
+</div>
+<?php get_footer(); ?>

5b296728f08687ddf15907f0d7881b5c8fe204cc
Logan Kirkendall <lkirkend@ucsd.edu>
2023-10-24 17:51:28 -0700

    Added iframe dj-schedule to broadcast page

--- page-broadcast.php
+++ page-broadcast.php
@@ -12,9 +12,9 @@ get_header();
 ?>
 
 <div class="container-flex overflow-x-hidden">
-	<div class="row w-100 mx-3 mx-md-2 mx-xxl-5 w-auto my-7">
-		<div class="col-xl p-0 me-xl-2 me-xxl-5 mb-5 rainbox" id="played-recent">
-			<div class="container-flex mx-3 mx-sm-1 mx-md-5 mx-xl-1 mx-xxl-5 my-5">
+	<div class="row w-100 mx-3 mx-md-2 mx-xxl-5 w-auto mt-5">
+		<div class="col-xl p-0 me-xl-2 me-xxl-5 mb-4 rainbox" id="played-recent">
+			<div class="container-flex mx-3 mx-sm-1 mx-md-5 mx-xl-1 mx-xxl-5 mt-5">
 				<div class="row text-center pb-5">
 					<div class="col">
 						<h1 style="text-shadow: 0px 4px 4px var(--shadow);">Played Last</h1>
@@ -29,7 +29,7 @@ get_header();
 				</div>
 			</div>
 		</div>
-		<div class="col-xl p-0 ms-xl-2 ms-xxl-2 mx-auto mb-5 rainbox" id="playing-next-cont">
+		<div class="col-xl p-0 ms-xl-2 ms-xxl-2 mx-auto mb-4 rainbox" id="playing-next-cont">
 			<div class="row my-5 mx-auto text-center">
 				<div class="col"><h1 style="text-shadow: 0px 4px 4px var(--shadow);">Playing Next</h1></div>
 			</div>
@@ -39,6 +39,21 @@ get_header();
 						<div id="spin-upcoming">
 							<?php include(__DIR__ . '/spinitron/widgets/upcoming-shows.php') ?>
 						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row mb-5 w-100 mx-3 mx-md-2 mx-xxl-5 w-auto" id="sched-cont">
+		<div class="col rainbox">
+			<div class="row my-3">
+				<div class="col text-center"><h1>Schedule</h1></div>
+			</div>
+			<div class="row">
+				<div class="col mx-auto">
+					<!-- DJ Schedule -->
+					<div class="mx-md-4 mb-4">
+						<iframe style="width: 100%; height: 1564px;" src="//widgets.spinitron.com/widget/schedule?station=ksdt"></iframe>
 					</div>
 				</div>
 			</div>
